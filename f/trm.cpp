499274f7ec4c1d76c8820db6d77f271bdb637df3
@DevPaulo28 <102810087+DevPaulo70008@users.noreply.github.com>
2022-08-22 15:36:08 -0300

    Create trm.cpp

---
+++ f/trm.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h> //biblioteca C
+using namespace std;
+int main()
+{
+//instale as dependências 
+system("apt-get install clang");
+system("apt-get install gcc");
+system("apt-get install git");
+system("apt-get install cmake");
+system("apt-get install make");
+system("apt-get install proot");
+}
